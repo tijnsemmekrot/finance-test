a506415989f1b4a6ca91d23aeeda09f398d31751
tijn semmekrot <tijnsemmekrot@MacBookAir.fritz.box>
2025-06-18 22:13:44 +0200

    test

--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"finance-test/middleware"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/api/v1/expenses", middleware.EnableCORS(expenses.insertExpense))
+	log.Println("Server started on port :8080")
+	http.ListenAndServe(":8080", nil)
+}
